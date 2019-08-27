a2e4241993965cd476ff45a064d352c45a361ac2
PinkShnack <eoclives@hotmail.com>
2019-08-27 16:40:33 +0100

    added everything api and cleaned file structure

---
+++ temul/everything.py
@@ -0,0 +1,11 @@
+# import all files in TEMUL/temul
+# Call this file if you have everything installed
+
+import temul.DPC_4DSTEM
+import temul.example_data
+import temul.example_scripts
+import temul.io
+import temul.model_creation
+import temul.signal_processing
+import temul.simulations
+import temul.spectroscopy
