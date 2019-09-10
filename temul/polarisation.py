359776e736dcd1019ba4b6b96409b32426bf8a00
PinkShnack <eoclives@hotmail.com>
2019-09-10 16:25:34 +0100

    added polarisation.py for domain wall functions

---
+++ temul/polarisation.py
@@ -0,0 +1,60 @@
+
+import atomap.api as am
+
+
+def delete_atom_planes_from_sublattice(sublattice,
+                                       zone_axis_index=0,
+                                       divisible_by=3,
+                                       offset_from_zero=0):
+    '''
+    delete atom_planes from a zone axis. Can choose whether to delete 
+    every second, third etc., and the offset from the zero index.
+
+    Parameters
+    ----------
+    sublattice : Atomap Sublattice object
+    zone_axis_index : int, default 0
+        The zone axis you wish to specify. You are indexing
+        sublattice.zones_axis_average_distances[zone_axis_index]
+    divisible_by : int, default 3
+        If divisible_by is 2, every second atom_plane is deleted,
+        If divisible_by is 4, every fourth atom_plane is deleted, etc.
+    offset_from_zero : int, default 0
+        The atom_plane from which you start deleting. 
+        If offset_from_zero is 4, the fourth atom_plane will be 
+        the first deleted.
+
+    Examples
+    --------
+
+    >>> atom_lattice = am.dummy_data.get_polarization_film_atom_lattice()
+    >>> sublatticeA = atom_lattice.sublattice_list[0]
+    >>> delete_atom_planes_from_sublattice(sublattice=sublatticeA,
+    ...                            zone_axis_index=0,
+    ...                            divisible_by=3,
+    ...                            offset_from_zero=1)
+    >>> sublatticeA.plot_planes()
+
+    '''
+    sublattice.construct_zone_axes()
+
+    zone_vec_needed = sublattice.zones_axis_average_distances[zone_axis_index]
+
+    atom_plane_index_delete = []
+    for i, atom_plane in enumerate(sublattice.atom_planes_by_zone_vector[zone_vec_needed]):
+        if i % divisible_by == 0:
+            atom_plane_index_delete.append(i)
+
+    # atom_plane_index_delete = [0, 3, 6, 9]
+    # offset_from_zero = 2
+    atom_plane_index_delete = [offset_from_zero +
+                               index for index in atom_plane_index_delete]
+    atom_plane_index_delete = [index for index in atom_plane_index_delete
+                               if index < len(sublattice.atom_planes_by_zone_vector[zone_vec_needed])]
+
+    # reversal needed because first it will delete 0, then 1 will become 0.
+    # Then it will delete 3, which is the wrong one! (should have been 2)
+    atom_plane_index_delete.sort(reverse=True)
+
+    for i in atom_plane_index_delete:
+        del sublattice.atom_planes_by_zone_vector[zone_vec_needed][i]
