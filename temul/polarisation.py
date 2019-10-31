dcd28d93bcd74226a259896a5c0cfc5d7f71f503
PinkShnack <eoclives@hotmail.com>
2019-10-31 19:34:16 +0000

    added functions for retrieving polarisation vectors from two atom lists and plotting them

--- temul/polarisation.py
+++ temul/polarisation.py
@@ -1,6 +1,117 @@
 
 import atomap.api as am
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+def find_polarisation_vectors(atom_positions_A, atom_positions_B,
+                              save='uv_vectors_array'):
+    '''
+
+    other params: unit vectoring,
+    '''
+    if len(atom_positions_A) != len(atom_positions_B):
+        raise ValueError("atom_positions_A and atom_positions_B must be the "
+                         "same length")
+
+    atom_positions_A_list_x = [row[0] for row in atom_positions_A]
+    atom_positions_A_list_y = [row[1] for row in atom_positions_A]
+
+    atom_positions_B_list_x = [row[0] for row in atom_positions_B]
+    atom_positions_B_list_y = [row[1] for row in atom_positions_B]
+
+    # Create a list of dx and dy by simple subtraction
+    u_v_component_list = []
+    i = 0
+    delta = 1
+    while i < len(atom_positions_B):
+        u_v_component = (
+            atom_positions_B_list_x[i] - atom_positions_A_list_x[i],
+            atom_positions_B_list_y[i] - atom_positions_A_list_y[i])
+
+        u_v_component_list.append(u_v_component)
+        i = i + delta
+
+    # Separate the created list into u (u=dx) and v (v=dy)
+        #   u and v are notation used by the ax.quiver plotting tool
+    u = [row[0] for row in u_v_component_list]
+    v = [row[1] for row in u_v_component_list]
+
+    if save is not None:
+        np.save(save + '.npy', u_v_component_list)
+
+    return(u, v)
+
+
+def plot_polarisation_vectors(u, v, sublattice, image=None,
+                              plot_style=['overlay'],
+                              save='polarisation_image',
+                              pivot='middle', color='yellow',
+                              angles='xy', scale_units='xy',
+                              scale=None, headwidth=3.0,
+                              width=0.005, headlength=5.0,
+                              headaxislength=4.5, title=""):
+
+    if image is None:
+        image = sublattice.image
+    else:
+        image = image
+
+    if "vectors" in plot_style:
+
+        _, ax = plt.subplots()
+        ax.quiver(
+            sublattice.x_position,
+            sublattice.y_position,
+            u,
+            v,
+            angles=angles,
+            scale_units=scale_units,
+            scale=scale,
+            width=width,
+            headwidth=headwidth,
+            headlength=headlength,
+            headaxislength=headaxislength,
+            pivot=pivot,
+            color=color)
+        ax.set(aspect='equal')
+        ax.set_xlim(0, len(image[:, 0]))
+        ax.set_ylim(len(image[0, :]), 0)
+        plt.title(title)
+        plt.tight_layout()
+        if save is not None:
+            plt.savefig(fname=save + '.png',
+                        transparent=True, frameon=False, bbox_inches='tight',
+                        pad_inches=None, dpi=300, labels=False)
+
+    if "overlay" in plot_style:
+        _, ax = plt.subplots()
+        ax.quiver(
+            sublattice.x_position,
+            sublattice.y_position,
+            u,
+            v,
+            angles=angles,
+            scale_units=scale_units,
+            scale=scale,
+            width=width,
+            headwidth=headwidth,
+            headlength=headlength,
+            headaxislength=headaxislength,
+            pivot=pivot,
+            color=color)
+        ax.set(aspect='equal')
+        ax.set_xlim(0, len(image[:, 0]))
+        ax.set_ylim(len(image[0, :]), 0)
+        plt.imshow(sublattice.image)
+        plt.gca().axes.get_xaxis().set_visible(False)
+        plt.gca().axes.get_yaxis().set_visible(False)
+        plt.title(title)
+        plt.tight_layout()
+        if save is not None:
+            plt.savefig(fname=save + '.png',
+                        transparent=True, frameon=False, bbox_inches='tight',
+                        pad_inches=None, dpi=300, labels=False)
 
 
 def get_vector_magnitudes(u, v, sampling=None):
@@ -105,11 +216,14 @@ def delete_atom_planes_from_sublattice(sublattice,
     atom_plane_index_delete = [offset_from_zero +
                                index for index in atom_plane_index_delete]
     atom_plane_index_delete = [index for index in atom_plane_index_delete
-                               if index < len(sublattice.atom_planes_by_zone_vector[zone_vec_needed])]
+                               if index < len(
+                                   sublattice.atom_planes_by_zone_vector[
+                                       zone_vec_needed])]
 
     if opposite:
         opposite_list = [
-            index for index in opposite_list if index not in atom_plane_index_delete]
+            index for index in opposite_list
+            if index not in atom_plane_index_delete]
         atom_plane_index_delete = opposite_list
     # reversal needed because first it will delete 0, then 1 will become 0.
     # Then it will delete 3, which is the wrong one! (should have been 2)
