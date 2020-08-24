728c02630563c454d0cc971aabea9aa4fece7c7c
PinkShnack <eoclives@hotmail.com>
2020-08-24 22:44:11 +0200

    import error fix

--- temul/signal_processing.py
+++ temul/signal_processing.py
@@ -4,8 +4,8 @@ from temul.external.atomap_devel_012.initial_position_finding import (
     add_atoms_with_gui as choose_points_on_image)
 
 from atomap.sublattice import Sublattice
-from atomap.initial_position_finding import get_atom_positions
-from atomap.atom_finding_refining import _make_circular_mask
+from atomap.atom_finding_refining import (
+    get_atom_positions, _make_circular_mask)
 import temul.external.atomap_devel_012.api as am_dev
 
 import matplotlib
